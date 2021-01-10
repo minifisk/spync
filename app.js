fb9907309144dc224d97e6dba01b206279e3000f
minifisk <73173943+minifisk@users.noreply.github.com>
2021-01-10 17:45:50 +0100

    serveruppdateringar

--- app.js
+++ app.js
@@ -6,18 +6,9 @@ const mysql = require('mysql');
 //express app
 const app = express();
 
-//Connect to mongoDB
-const dbURI = 'mongodb+srv://netninja:test1234@cluster0.1ztr7.mongodb.net/<dbname>?retryWrites=true&w=majority'; 
-
 
 //Register view engine
 app.set('view engine', 'ejs');
-
-// Listen for requests
-const port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log('listening to: ', port);
-});
 
 //Middleware and static files
 app.use(express.static('public'));
@@ -45,11 +36,20 @@ app.use((req, res) => {
     res.status(404).render('404')
 })
 
+/* // Connect to database
+var connection = mysql.createConnection({
+    host     : "spync-database-instance-1.csydz3ayt58b.us-east-2.rds.amazonaws.com",
+    user     : "admin",
+    password : "AHBhd8Fr&Afu",
+    port     : "8080"
+  }); */
+
+  // Connect to database
 var connection = mysql.createConnection({
     host     : process.env.RDS_HOSTNAME,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
+    port     : process.env.RDS_PORT 
   });
   
   connection.connect(function(err) {
@@ -62,3 +62,9 @@ var connection = mysql.createConnection({
   });
   
   connection.end();
+
+  // Listen for requests
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('listening to: ', port);
+});
