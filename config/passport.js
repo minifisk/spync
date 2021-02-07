fba78bbf78bfbcf75b651f6422c8d57e06e18c03
minifisk <73173943+minifisk@users.noreply.github.com>
2021-02-07 17:28:04 +0100

    Updated comments

--- config/passport.js
+++ config/passport.js
@@ -31,12 +31,7 @@ module.exports = function(passport) {
         aUser.accessToken = accessToken;
         aUser.refreshToken = refreshToken;
       
-      // NOTE: don't run the code below async
-      /* RUN ALL CODE BELOW THIS IN userHandler Async function (to keep track of
-        user_id throughout all coming calls). This will first check if user
-        is in db, if not add it, and return it's user_id for coming calls, if
-        it is in db, it will simply return it's user_id to be used in coming
-        calls */
+      // NOTE: don't run the code below async - thanks to @blahah
 
       /* Check if user is in DB, if not, add. Store user_id in "user" for
       using in other calls */
@@ -47,32 +42,30 @@ module.exports = function(passport) {
       was created 30 days or longer ago. If true, delete all rows
       in both tables and then run code below. If false, return from function */
 
-      let preferencesStatus = await checkPreferences(user_id)
+      let preferencesStatus = await checkPreferences(user_id);
       
-      // If user preferences was older than 30 days and checkPreferences
-      // deleted the tables, run the folliwing code:
+      /* If user preferences was older than 30 days and checkPreferences
+      deleted the tables, run the folliwing code: */
       if (preferencesStatus === true) {
         
         /* SET UP SPOTIFY API CALL TO GET TOP STATS FROM USER */
 
-        // Initialize spotifyapi object
+        /* Initialize spotifyapi object to prepare to retrieve top songs and artists */
         var spotifyApi = new SpotifyWebApi({
           clientID: process.env.SPOTIFY_CLIENT_ID,
           clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
           callbackURL: process.env.SPOTIFY_CALLBACK_URL
         });
 
-        // Set accesstoken for api objct
-
+        /* Set accesstoken for api object  */
         spotifyApi.setAccessToken(accessToken);
       
         /* STEP 1: RETRIEVE TOP ARTISTS FOR CURRENT USER */
-
         await spotifyApi.getMyTopArtists({ limit: 5 })
           .then(function(data) {
             let topArtistsRaw = data.body.items;
           
-            // Map top artists and key data into a new array
+            /* Map top artists and key data into a new array */
             let mappedArtists = topArtistsRaw.map(artist => {
               return {
               artist: artist.name, 
@@ -82,15 +75,15 @@ module.exports = function(passport) {
               }
             });
 
-            // Map through each favorite artist of the user
+            /* Map through each favorite artist of the user from the array */
             mappedArtists.map(async artist => {
               let a_artist = artist.artist;
               let a_popularity = artist.popularity;
               let a_url = artist.url;
               let a_image = artist.image;
 
-              // Check if artist is in the general artist-table, if not add
-              // Store the artist_id in variable
+              /* Check if artist is in the general artist-table, if not add it.
+              Then store the returning artist_id in artist_id */
               let artist_id = await checkArtist(a_artist, a_url, a_popularity, a_image);
 
               // Add the artist to current users artistPreferences table
@@ -101,10 +94,9 @@ module.exports = function(passport) {
         /* STEP 2: RETRIEVE TOP TRACKS FOR CURRENT USER */
         await spotifyApi.getMyTopTracks({ limit: 2 })
           .then(function(data) {
-          
             let topTracksRaw = data.body.items;
 
-            // Map key data for each track into new array
+            /* Map key data for each track into new array */
             let mappedTracks = topTracksRaw.map(track => {
               return {
               trackname: track.name, 
@@ -114,29 +106,30 @@ module.exports = function(passport) {
               }
             });
 
-            // Check if each track is in the DB, if not add
+            /* Check if each track is in the DB, if not add the track to DB */
             mappedTracks.map(async track => {
               let a_track = track.trackname;
               let a_artist = track.artist;
               let a_url = track.url;
               let a_popularity = track.popularity;
             
-              // Check if current track is in the collective artist/track DB
-              // Store track_id in variable
+              /* Check if current track is in the collective artist/track DB
+              Store the reeturning track_id in variable */
               let track_id = await checkTrack(a_track, a_artist, a_url, a_popularity);
         
-              // Add the user trackPreferences DB's
+              /* Add the user trackPreferences to DB's */
               await addTrackPreferences(user_id, track_id, a_artist);
             });
           });
         }
-
+        /* Reach this at the end of updating DB or directly if the user
+        preferences are up to date */
         return done(null, profile);
       }     
     )
   );
 
-  // Serialize
+  /* Serialize user to be used in sessions */
   passport.serializeUser(function(user, done) {
     done(null, user);
   });
